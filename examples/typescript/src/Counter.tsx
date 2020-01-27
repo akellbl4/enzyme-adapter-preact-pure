854fadc39be5e2b3aeef72df2276ef2d313c2bf9
Pavel Mineev <pavel@mineev.me>
2020-01-28 02:18:17 +0300

    Problem

--- examples/typescript/src/Counter.tsx
+++ examples/typescript/src/Counter.tsx
@@ -1,13 +1,18 @@
 import * as preact from 'preact';
 import { useState } from 'preact/hooks';
 
-export default function Counter({ initialCount } : { initialCount: number }) {
+export default function Counter({ initialCount }: { initialCount: number }) {
   const [count, setCount] = useState(initialCount);
   const increment = () => setCount(count + 1);
+  const handleChange = (e: Event) => {
+    const { value } = e.target as HTMLInputElement;
+
+    setCount(Number(value));
+  };
 
   return (
     <div>
-      Current value: {count}
+      Current value: <input type="text" value={count} onInput={handleChange} />
       <button onClick={increment}>Increment</button>
     </div>
   );
--- examples/typescript/test/Counter-test.tsx
+++ examples/typescript/test/Counter-test.tsx
@@ -7,7 +7,7 @@ import Counter from '../src/Counter';
 describe('Counter', () => {
   it('should display initial count', () => {
     const wrapper = mount(<Counter initialCount={5} />);
-    expect(wrapper.text()).to.include('Current value: 5');
+    expect(wrapper.find('input').prop('value')).to.equal(5);
   });
 
   it('should render a button', () => {
@@ -23,6 +23,14 @@ describe('Counter', () => {
 
     wrapper.find('button').simulate('click');
 
-    expect(wrapper.text()).to.include('Current value: 6');
+    expect(wrapper.find('input').prop('value')).to.equal(6);
+  });
+
+  it('should change value by input', () => {
+    const wrapper = mount(<Counter initialCount={5} />);
+
+    wrapper.find('input').simulate('input', { target: { value: '1' } });
+
+    expect(wrapper.find('input').prop('value')).to.equal(1);
   });
 });
